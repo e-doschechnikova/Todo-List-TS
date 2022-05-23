f3c5a0adb6fd9a295d919fd8763bc3c10327f435
e-doschechnikova <evadrobakova@gmail.com>
2022-05-23 11:42:48 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+import {TaskType} from "./Todolist";
+
+function App() {
+
+    let tasks1: Array<TaskType> = [
+        {id: 1, title: "CSS", isDone: false},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: true}
+    ]
+
+    let tasks2: Array<TaskType> = [
+        {id: 1, title: "The Notebook", isDone: true},
+        {id: 2, title: "The Great Gatsby", isDone: true},
+        {id: 3, title: "The Age of Adaline", isDone: true}
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Movies" tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
