2570d00e1a10b73264e6ecbf8970b6ca03b43f7c
e-doschechnikova <evadrobakova@gmail.com>
2022-05-27 15:45:43 +0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Todolist } from "./Todolist";
 import { TasksPropsType } from "./Todolist";
 
+const useState2 = (data: any) => {
+  return [data, () => {}];
+};
+
+let arr = useState2([{}, {}, {}]);
+
+let tasks = arr[0];
+let setTasks = arr[1];
+
 function App() {
-  const tasks1: Array<TasksPropsType> = [
-    { id: 1, title: "CSS", isDone: false },
+  let initTasks: Array<TasksPropsType> = [
+    { id: 1, title: "CSS", isDone: true },
     { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "React", isDone: true },
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Redux", isDone: false },
   ];
 
-  const tasks2: Array<TasksPropsType> = [
-    { id: 1, title: "The Notebook", isDone: false },
-    { id: 2, title: "The Great Gatsby", isDone: false },
-    { id: 3, title: "The Age of Adaline", isDone: true },
-  ];
+  let arr = useState(initTasks);
 
-  return (
-    <div className="App">
-      <Todolist title="What to learn" tasks={tasks1} />
-      <Todolist title="What to watch" tasks={tasks2} />
-    </div>
-  );
+  let tasks = arr[0];
+  let setTasks = arr[1];
+
+  const removeTask = (id: number) => {
+    let filteredTasks = tasks.filter((el) => el.id !== id);
+    setTasks(filteredTasks);
+
+    // const tasks2: Array<TasksPropsType> = [
+    //   // { id: 1, title: "The Notebook", isDone: false },
+    //   { id: 2, title: "The Great Gatsby", isDone: false },
+    //   { id: 3, title: "The Age of Adaline", isDone: true },
+    // ];
+
+    return (
+      <div className="App">
+        <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} />
+        {/* <Todolist title="What to watch" tasks={tasks2}/> */}
+      </div>
+    );
+  };
 }
-
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,36 +1,48 @@
 import React from "react";
 
 type TodolistPropsType = {
-    title: string,
-    tasks: Array<TasksPropsType>
-}
+  title: string;
+  tasks: Array<TasksPropsType>;
+  removeTask: Function;
+};
 
 export type TasksPropsType = {
-    id: number
-    title: string
-    isDone: boolean
-}
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 
-export function Todolist(props: TodolistPropsType) { // props = { title: "What to learn", tasks: []
-    return (
-        <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {props.tasks.map(el => {
-                    return (
-                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-                    )
-                })}
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>)
-}
-
+export const Todolist = (props: TodolistPropsType) => {
+  // props = { title: "What to learn", tasks: []
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {props.tasks.map((el) => {
+          return (
+            <li>
+              <input type="checkbox" checked={el.isDone} />{" "}
+              <span>{el.title}</span>
+              <button
+                onClick={() => {
+                  props.removeTask(el.id);
+                }}
+              >
+                x
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
