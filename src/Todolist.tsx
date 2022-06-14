3970931622ee1c15303b6aeb34bcb7ddae8f4e14
e-doschechnikova <evadrobakova@gmail.com>
2022-06-14 21:35:24 +0300

    add styles

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 import { FilterValuesType } from "./App";
+import styles from "./Todolist.module.css";
 
 type TaskType = {
   id: string;
@@ -17,34 +18,29 @@ type PropsType = {
 };
 
 export function Todolist(props: PropsType) {
+  const [error, setError] = useState(false);
   let [title, setTitle] = useState("");
 
   const addTask = () => {
     if (title.trim() !== "") {
       props.addTask(title.trim());
       setTitle("");
+    } else {
+      setError(true);
     }
   };
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.currentTarget.value);
   };
-
   const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.charCode === 13) {
       addTask();
     }
   };
-
   const onAllClickHandler = () => props.changeFilter("all");
   const onActiveClickHandler = () => props.changeFilter("active");
   const onCompletedClickHandler = () => props.changeFilter("completed");
-
-  // const changeCheckBoxHandler = (
-  //   tID: string,
-  //   event: ChangeEvent<HTMLInputElement>
-  // ) => props.changeCheckBox(tID, event.currentTarget.checked);
-
   const changeCheckBoxHandler = (tID: string, eventValue: boolean) =>
     props.changeCheckBox(tID, eventValue);
 
@@ -53,11 +49,13 @@ export function Todolist(props: PropsType) {
       <h3>{props.title}</h3>
       <div>
         <input
+          className={error ? styles.error : ""}
           value={title}
           onChange={onChangeHandler}
           onKeyPress={onKeyPressHandler}
         />
         <button onClick={addTask}>+</button>
+        {error && <div className={styles.errorMessage}>Title is requred</div>}
       </div>
       <ul>
         {props.tasks.map((t) => {
