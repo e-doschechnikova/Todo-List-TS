77be984aecab9f713c07956e6eb1e7341b286050
e-doschechnikova <evadrobakova@gmail.com>
2022-07-12 19:29:13 +0300

    add reducers and tests

--- src/reducers/todolists-reducer.test.ts
+++ src/reducers/todolists-reducer.test.ts
@@ -1,5 +1,5 @@
 import { todolistsReducer } from "./todolists-reducer";
-import { TodoListType } from "./../App";
+import { TodoListType } from "../App";
 import { v1 } from "uuid";
 
 test("correct todolist should be removed", () => {
@@ -19,4 +19,25 @@ test("correct todolist should be removed", () => {
   // cверка результата c ожиданием:
   expect(endState.length).toBe(1);
   expect(endState[0].id).toBe(todolistId1);
+});
+
+test("correct todolist should be added", () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newTodolistTitle = "New Todolist";
+
+  const startState: Array<TodoListType> = [
+    { id: todolistId1, title: "What to learn", filter: "all" },
+    { id: todolistId2, title: "What to buy", filter: "all" },
+  ];
+
+  const endState = todolistsReducer(startState, {
+    type: "ADD-TODOLIST",
+    title: newTodolistTitle,
+    id: v1(),
+  });
+
+  expect(endState.length).toBe(3);
+  expect(endState[2].title).toBe(newTodolistTitle);
 });
--- src/reducers/todolists-reducer.ts
+++ src/reducers/todolists-reducer.ts
@@ -1,17 +1,35 @@
+import { v1 } from "uuid";
 import { TodoListType } from "../App";
+
+///-----------type for action type -----------\\\
+type AddTodoListAT = {
+  type: "ADD-TODOLIST";
+  title: string;
+  id: string;
+};
 
 type RemoveTodoListAT = {
   type: "REMOVE-TODOLIST";
   id: string;
 };
 
+///-------------------------------------------\\\
+
+type ActionType = RemoveTodoListAT | AddTodoListAT;
+
 export const todolistsReducer = (
   todolists: Array<TodoListType>,
-  action: RemoveTodoListAT
+  action: ActionType
 ): Array<TodoListType> => {
   switch (action.type) {
+    case "ADD-TODOLIST":
+      return [
+        ...todolists,
+        { id: action.id, title: action.title, filter: "all" },
+      ];
     case "REMOVE-TODOLIST":
       return todolists.filter((tl) => tl.id !== action.id);
+
     default:
       return todolists;
   }
