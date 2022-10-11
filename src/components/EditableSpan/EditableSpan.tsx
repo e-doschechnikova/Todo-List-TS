d7dd004816776b44f5b7a2793cfe888d5e3acea9
e-doschechnikova <evadrobakova@gmail.com>
2022-10-11 16:08:24 +0300

    refactoring component EditableSpan

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -1,44 +1,41 @@
-import { TextField } from "@material-ui/core";
-import React, { ChangeEvent, FC, useState, KeyboardEvent } from "react";
+import {TextField} from "@material-ui/core";
+import React, {ChangeEvent, useState, KeyboardEvent, memo} from "react";
 
 type EditableSpanPropsType = {
-  title: string;
-  changeTitle: (editedTitle: string) => void;
+    title: string;
+    changeTitle: (editedTitle: string) => void;
 };
 
-export const EditableSpan: FC<EditableSpanPropsType> = ({
-  title,
-  changeTitle,
-}) => {
-  const [text, setText] = React.useState<string>(title);
-  const [editMode, setEditMode] = useState<boolean>(false);
+export const EditableSpan = memo(({title, changeTitle}: EditableSpanPropsType) => {
+    const [text, setText] = React.useState<string>(title);
+    const [editMode, setEditMode] = useState<boolean>(false);
 
-  const onChangeSetText = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.currentTarget.value);
-  };
+    const onChangeSetText = (e: ChangeEvent<HTMLInputElement>) => {
+        setText(e.currentTarget.value);
+    };
 
-  const onKeyDownChangeTitle = (e: KeyboardEvent<HTMLInputElement>) =>
-    e.key === "Enter" && offEditMode();
+    const onKeyDownChangeTitle = (e: KeyboardEvent<HTMLInputElement>) =>
+        e.key === "Enter" && offEditMode();
 
-  const onEditMode = () => {
-    setEditMode(true);
-  };
+    const onEditMode = () => {
+        setEditMode(true);
+    };
 
-  const offEditMode = () => {
-    setEditMode(false);
-    changeTitle(text);
-  };
+    const offEditMode = () => {
+        setEditMode(false);
+        changeTitle(text);
+    };
 
-  return editMode ? (
-    <TextField
-      color={"primary"}
-      value={text}
-      onChange={onChangeSetText}
-      onKeyDown={onKeyDownChangeTitle}
-      onBlur={offEditMode}
-      autoFocus
-    />
-  ) : (
-    <span onDoubleClick={onEditMode}>{title}</span>
-  );
-};
+    return editMode ? (
+        <TextField
+            color={"primary"}
+            value={text}
+            onChange={onChangeSetText}
+            onKeyDown={onKeyDownChangeTitle}
+            onBlur={offEditMode}
+            autoFocus
+        />
+    ) : (
+        <span onDoubleClick={onEditMode}>{title}</span>
+    );
+});
