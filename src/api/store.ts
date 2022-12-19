96d3d55ef635e3d670417956613c9a7270712fbe
e-doschechnikova <evadrobakova@gmail.com>
2022-12-19 17:02:24 +0300

    add: redux toolkit. refactoring: store

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,10 +1,11 @@
 import {tasksReducer} from "../features/TodolistsList/Todolist/Task/tasks-reducer";
 import {todolistsReducer} from "../features/TodolistsList/Todolist/todolists-reducer";
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import {AnyAction, combineReducers} from "redux";
 import thunk, {ThunkDispatch} from "redux-thunk"
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {appReducer} from "../app/app-reducer";
 import {authReducer} from "../features/Login/auth-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -13,7 +14,12 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+// export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
+})
 export const useAppDispatch = () => useDispatch<AppDispatchType>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 // @ts-ignore
